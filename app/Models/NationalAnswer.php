84d22dd5d6cad102e71aedd9ba506f98a59ddd92
AyaAlMohmad <84016184+AyaAlMohmad@users.noreply.github.com>
2023-09-02 22:10:21 +0100

    Update NationalAnswer.php

--- app/Models/NationalAnswer.php
+++ app/Models/NationalAnswer.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class NationalAnswer extends Model
 {
     use HasFactory ;
+        use SoftDeletes;
+
     // ,HasUuids
     protected $fillable=['answer','national_question_id','status','uuid'];
 
