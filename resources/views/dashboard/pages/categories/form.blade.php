b0c9678d034fdd4486f3c70078bd623ffccf3f15
AyaAlMohmad <84016184+AyaAlMohmad@users.noreply.github.com>
2023-09-02 22:22:57 +0100

    Update form.blade.php

--- resources/views/dashboard/pages/categories/form.blade.php
+++ resources/views/dashboard/pages/categories/form.blade.php
@@ -2,56 +2,45 @@
 
 
 @section('form_input')
-<a href="{{ route('category.index') }}" class="btn btn-primary btn-xs">Category</a>
-<form class="form-horizontal" role="form" method="POST" enctype="multipart/form-data"
-    action="{{ route('category.store') }}">
-    @csrf
-    <div class="widget-main">
-        <div class="tab-content padding-4">
-            <div class="form-group">
+    <a href="{{ route('category.index') }}" class="btn btn-primary btn-xs">category</a>
 
-                <label class="col-sm-2 control-label no-padding-right" for="name">
+    <form class="form-horizontal" role="form" method="POST" enctype="multipart/form-data"
+        action="{{ route('category.store') }}">
+        @csrf
 
-                   Name
-                </label>
-                <div class="col-sm-4">
-                    <input class="col-xs-10 col-sm-5" type="text" 
-                        name="name" id="name" >
-                    @error('name')
-                        <div>{{ $message }}</div>
-                    @enderror
-                </div>
+
+        <div class="widget-main">
+
+            <div class="tab-content padding-4">
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label no-padding-right" for="name">
+
+                        Name
+                    </label>
+                    <div class="col-sm-4">
+                        <input class="col-xs-10 col-sm-5" type="text" name="name" id="name">
+                        @error('name')
+                            <div>{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div><br><br>
+
             </div><br><br>
 
+
+
+
+
+
+
+
+            <div class="form-group">
+                <div class="col-sm-6 control-label no-padding-right">
+                    <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-save"></i>save</button>
+                </div>
+            </div>
+
         </div>
-
-     
-    </div><br><br>
-
-
-    <div class="form-group">
-        <div class="col-sm-6 control-label no-padding-right">
-            <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-save"></i>save</button>
-        </div>
-    </div>
-
-    </div>
-</form>
-
-
-
-    <script>
-        function previewFile(input) {
-            var file = $("input[type=file]").get(0).files[0];
-            if (file) {
-                var reader = new FileReader();
-                reader.onload = function() {
-                    $("#previewImage").attr("src", reader.result);
-                }
-                reader.readAsDataURL(file);
-
-
-            }
-        }
-    </script>
+    </form>
 @endsection
