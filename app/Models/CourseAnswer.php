58e81b2705f11015641d65c2dfda399f58809526
AyaAlMohmad <84016184+AyaAlMohmad@users.noreply.github.com>
2023-09-02 21:51:31 +0100

    Update CourseAnswer.php

--- app/Models/CourseAnswer.php
+++ app/Models/CourseAnswer.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class CourseAnswer extends Model
 {
     use HasFactory;
+        use SoftDeletes;
+
     // ,HasUuids
     protected $fillable=['answer','uuid'];
     
