f4211456ae11a4afd5e081abeb4b759ef472ccef
AyaAlMohmad <84016184+AyaAlMohmad@users.noreply.github.com>
2023-09-02 22:21:31 +0100

    Update edit.blade.php

--- resources/views/dashboard/pages/course/edit.blade.php
+++ resources/views/dashboard/pages/course/edit.blade.php
@@ -2,10 +2,10 @@
 
 
 @section('form_input')
-    <a href="{{ route('courseAnswers.index') }}" class="btn btn-primary btn-xs">courseAnswers</a>
+    <a href="{{ route('course.index') }}" class="btn btn-primary btn-xs">course</a>
 
     <form class="form-horizontal" role="form" method="POST" enctype="multipart/form-data"
-        action="{{ route('courseAnswers.update', $Answer->uuid) }}">
+        action="{{ route('course.update', $course->uuid) }}">
         @csrf
         @method('put')
 
@@ -14,46 +14,35 @@
             <div class="tab-content padding-4">
 
                 <div class="form-group">
-                    <label class="col-sm-2 control-label no-padding-right" for="answer">
+                    <label class="col-sm-2 control-label no-padding-right" for="name">
 
-                        Answer
+                        Name
                     </label>
                     <div class="col-sm-4">
-                        <input class="col-xs-10 col-sm-5" type="text" name="answer" id="answer" value="{{ $Answer->answer }}">
-                        @error('answer')
+                        <input class="col-xs-10 col-sm-5" type="text" name="name" id="name " value="{{ $course->name }}">
+                        @error('name')
                             <div>{{ $message }}</div>
                         @enderror
                     </div>
                 </div><br><br>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label no-padding-right" for="status">Status</label>
+                    <label class="col-sm-2 control-label no-padding-right" for="specialization_id">specialization</label>
                     <div class="col-sm-4">
-                        <select class="col-xs-10 col-sm-5" name="status" id="status">
-                            <option value="">Select a status</option>
-
-                            <option value="0">False</option>
-                            <option value="1">True</option>
-
+                        <select class="col-xs-10 col-sm-5" name="specialization_id" id="specialization_id">
+                            <option value="">{{ $course->spacialization->name }}</option>
+                         @foreach ($specialization as $item)
+                         <option value="{{ $item->id }}">{{ $item->name }}</option>
+                         @endforeach
+                           
+                                
+                          
                         </select>
-                        @error('question_id')
+                        @error('specialization_id')
                             <div>{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label no-padding-right" for="question_id">Question Name</label>
-                    <div class="col-sm-4">
-                        <select class="col-xs-10 col-sm-5" name="question_id" id="question_id">
-                            <option value="">Select a question</option>
-                            @foreach ($question as $item)
-                                <option value="{{ $item->id }}">{{ $item->question }}</option>
-                            @endforeach
-                        </select>
-                        @error('question_id')
-                            <div>{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
+                
             </div><br><br>
 
 
